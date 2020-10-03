c533ee7e1077629e2a4b35ec67d129fda4b75e4b
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-10-02 21:52:18 -0300

    Initial Commit

---
+++ PaymentApplication/PaymentApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentApplicationApp.swift
+//  PaymentApplication
+//
+//  Created by Raphael Cerqueira on 02/10/20.
+//
+
+import SwiftUI
+
+@main
+struct PaymentApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
